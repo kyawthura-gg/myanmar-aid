473606adbb96d310f002dbc14331a930132218dc
Kyaw Thura <kanaiyaaryal@gmail.com>
2025-04-01 23:10:34 +0700

    use trpc

--- db.ts
+++ db.ts
@@ -1,9 +1,9 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-import { PrismaD1 } from "@prisma/adapter-d1";
-import { PrismaClient } from "@prisma/client";
+import { getCloudflareContext } from "@opennextjs/cloudflare"
+import { PrismaD1 } from "@prisma/adapter-d1"
+import { PrismaClient } from "@prisma/client"
 
-export const db = async () => {
-  const ctx = await getCloudflareContext({ async: true });
-  const adapter = new PrismaD1((ctx.env as Cloudflare.Env).DB);
-  return new PrismaClient({ adapter });
-};
+export const getDB = async () => {
+	const ctx = await getCloudflareContext({ async: true })
+	const adapter = new PrismaD1((ctx.env as Cloudflare.Env).DB)
+	return new PrismaClient({ adapter })
+}
---
+++ server/routers/post.ts
@@ -0,0 +1,33 @@
+import { z } from "zod"
+
+import {
+	createTRPCRouter,
+	protectedProcedure,
+	publicProcedure,
+} from "@/server/trpc"
+
+export const postRouter = createTRPCRouter({
+	hello: publicProcedure
+		.input(z.object({ text: z.string() }))
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input.text}`,
+			}
+		}),
+
+	create: protectedProcedure
+		.input(z.object({ name: z.string().min(1) }))
+		.mutation(async ({ ctx, input }) => {
+			await ctx.db.post.create({ data: { name: input.name } })
+		}),
+
+	getLatest: protectedProcedure.query(async ({ ctx }) => {
+		const post = await ctx.db.post.findFirst()
+
+		return post ?? null
+	}),
+
+	getSecretMessage: protectedProcedure.query(() => {
+		return "you can now see this secret message!"
+	}),
+})
