a7a2133fb1900917f4ab79c28c2c8e46517efbc4
Kyaw Thura <kanaiyaaryal@gmail.com>
2025-04-05 22:46:13 +0700

    chore: debug prod auth issue

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -3,12 +3,14 @@ import { getCloudflareContext } from "@opennextjs/cloudflare"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
+  console.log("BETTER_AUTH_URL", process.env.BETTER_AUTH_URL)
   const ctx = await getCloudflareContext({ async: true })
   const auth = getAuth((ctx.env as Cloudflare.Env).DB)
   return auth.handler(request)
 }
 
 export async function POST(request: NextRequest) {
+  console.log("BETTER_AUTH_URL", process.env.BETTER_AUTH_URL)
   const ctx = await getCloudflareContext({ async: true })
   const auth = getAuth((ctx.env as Cloudflare.Env).DB)
   return auth.handler(request)
